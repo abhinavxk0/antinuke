c6344de80308a6de0057b61579c42323e3a8dff4
xavierk09 <xaviervavierk@gmail.com>
2021-08-13 17:14:38 +0530

    First Commit

---
+++ commands/commands 1/ping.js
@@ -0,0 +1,28 @@
+const moment = require('moment');
+
+module.exports = {
+    name: 'ping',
+    async execute(client, command, message, args, Discord){
+        const generalAnti = client.channels.cache.get('875701174450397204')
+        const errorLogs = client.channels.cache.get('875700619506241546')
+
+        const d = moment.duration(client.uptime);
+        const days = (d.days() == 1) ? `${d.days()} day` : `${d.days()} days`;
+        const hours = (d.hours() == 1) ? `${d.hours()} hour` : `${d.hours()} hours`;
+        const seconds = (d.seconds() == 1) ? `${d.seconds()} seconds` : `${d.seconds()} seconds`;
+        const minutes = (d.minutes() == 1) ? `${d.minutes()} minutes` : `${d.minutes()} minutes`;
+        if (message.author.id !== '776097608933441568') return 
+
+        try {
+            generalAnti.send(
+                    new Discord.MessageEmbed()
+                        .setTitle('Pong!')
+                        .setDescription(`WS Ping: ${Math.round(client.ws.ping)}ms\nUptime: ${days}, ${hours}, ${minutes} and ${seconds}`)
+                        .setColor('#00FFFF')
+            )
+        } catch (err) {
+            errorLogs.send(err)
+        }
+        
+    }
+}
